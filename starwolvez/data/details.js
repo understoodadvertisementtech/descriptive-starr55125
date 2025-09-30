6024b3dc91528194cb14ea05b3f6dd29275531b4
understood.advertisement <jxrtkufmfrtmdrt@gmail.com>
2025-10-01 02:39:12 +0800

    sync upload folder

---
+++ starwolvez/data/details.js
@@ -0,0 +1,118 @@
+// FOR EDITING
+
+    const isNFT = true; // false if not NFT
+    const ticker = "Pioneer Legends"; // Skip if not NFT
+    const serverName = "Star Wolvez";
+    const serverDescription = "8880 Wolvez have ascended from Planet Earth to find a better life in the Infinite Beyond.";
+    const serverWebsite = "https://www.starwolvez.com/"; // include https://
+    const serverTwitter = "https://x.com/starwolvez"; // include https://
+    const serverDiscord = "https://discord.gg/starwolvez"; // include https://
+    const serverChain = "ethereum";
+    // serverChain choices = abstract, apechain, arbitrum, avax, base, berachain, blast, bsc, ethereum, flow, monad, optimism, polygon, ronin, sei, shape, solana, soneium, unichain, zora
+    const explorerLink = "https://etherscan.io/address/0xd098c59d5d4a1ac3909f6093d0cf565314ecd240";
+    const gifImages = [
+   "https://img-cdn.magiceden.dev/da:t/rs:fill:400:0:0/plain/https%3A%2F%2Fimg.reservoir.tools%2Fimages%2Fv2%2Fmainnet%2Fi9YO%252F4yHXUdJsWcTqhqvf5vkqyYW%252FxaD43dJXasndySy0umJ3w9u%252F%252FP89K1HBghLdTS%252BH5EfOWMHMNtlWF3l%252F%252BtqdD0%252B4Vk48Sr0cfbyMB%252FOGRBJIY2s3KdFxPOEGLie.png",
+   "https://img-cdn.magiceden.dev/da:t/rs:fill:400:0:0/plain/https%3A%2F%2Fimg.reservoir.tools%2Fimages%2Fv2%2Fmainnet%2Fi9YO%252F4yHXUdJsWcTqhqvfwhR%252FNrFO%252Fmw1paCIvhv9us3Se5JO5R9gFAO6G4j8Cv3%252B1DuBtRqVdZRRm1QA6f0%252FZO54UG0UPTWjYz6qTQAT2ckKV9G67NWzjd44jhFi9nZ.png",
+   "https://img-cdn.magiceden.dev/da:t/rs:fill:400:0:0/plain/https%3A%2F%2Fimg.reservoir.tools%2Fimages%2Fv2%2Fmainnet%2Fi9YO%252F4yHXUdJsWcTqhqvfwRx%252BeCfxygOkd7vpbJpgYDIsOD5%252Bz6KIFitZhdy1ipbMkmCLJnU%252B%252FJF6rCLPawX6%252F9%252FkWDncV21Wdd9Bj4PcBwEsU92LyKmg5iNuR0AW376.png",
+   "https://img-cdn.magiceden.dev/da:t/rs:fill:400:0:0/plain/https%3A%2F%2Fimg.reservoir.tools%2Fimages%2Fv2%2Fmainnet%2Fi9YO%252F4yHXUdJsWcTqhqvf8U81ejsIN%252Fc1Mapkd3k9YmQ2crwMeRkLRWx81SfaloUOJGpB9N%252Br52TzOWZ4ma7oMz7rGRRp4wq9l76DzY%252FSBPZgIG1mqsql5HAeMpjlOxu.png",
+   "https://img-cdn.magiceden.dev/da:t/rs:fill:400:0:0/plain/https%3A%2F%2Fimg.reservoir.tools%2Fimages%2Fv2%2Fmainnet%2Fi9YO%252F4yHXUdJsWcTqhqvf8tcY6LraQlE51KJlIR9QZr2vm8tB%252BWSTkYJPIfB6e2YIKh0Hu59w%252FecF2aQgSgjYmPcXpIZCo8uUaTlsgXzPfN%252B7xLaIAdE5HzWVeZKIpky.png",
+
+    ];
+
+
+// END OF EDIT
+
+
+
+
+
+
+
+// DO NOT TOUCH
+
+    document.querySelectorAll(".replace-server").forEach(el => {
+        el.innerText = serverName;
+    });
+
+    document.getElementById("replaceDescription").innerText = serverDescription;
+
+    const websiteLink = document.getElementById("replaceWebsite");
+    websiteLink.innerText = new URL(serverWebsite).hostname;
+    websiteLink.href = serverWebsite;
+
+    const twitterLink = document.getElementById("replaceTwitter");
+    const twitterUrl = new URL(serverTwitter);
+    twitterLink.innerText = twitterUrl.hostname + twitterUrl.pathname;
+    twitterLink.href = serverTwitter;
+
+    const discordLink = document.getElementById("replaceDiscord");
+    const discordUrl = new URL(serverDiscord);
+    discordLink.innerText = discordUrl.hostname + discordUrl.pathname;
+    discordLink.href = serverDiscord;
+
+    const explorerUrl = document.getElementById("explorerLink");
+    explorerUrl.href = explorerLink;
+
+    let index = 0;
+    const img = document.getElementById("nftGIF");
+
+    setInterval(() => {
+        img.src = gifImages[index];
+        index = (index + 1) % gifImages.length;
+    }, 150);
+
+    const chainLogos = {
+        abstract: "https://abscan.org/assets/generic/html/favicon-light.ico",
+        apechain: "https://static1.tokenterminal.com//apechain/logo.png?logo_hash=0bcf0b91beec0e9cc66be298dcab89245320825b",
+        arbitrum: "https://guild.xyz/networkLogos/arbitrum.svg",
+        avax: "https://guild.xyz/explorerLogos/snowtrace.svg",
+        base: "https://guild.xyz/networkLogos/base.svg",
+        berachain: "https://berascan.com/assets/generic/html/favicon-light.ico",
+        blast: "https://blastscan.io/assets/blast/images/favicon-light.ico?v=25.7.4.1",
+        bsc: "https://guild.xyz/explorerLogos/bscscan-dark.svg",
+        ethereum: "https://guild.xyz/explorerLogos/etherscan-dark.svg",
+        flow: "https://www.flowscan.io/logo/flowscan-logo.svg",
+        monad: "https://cdn.prod.website-files.com/667c57e6f9254a4b6d914440/667f1590ccceec3eee19ec7c_logo.jpg",
+        optimism: "https://guild.xyz/networkLogos/optimism.svg",
+        polygon: "https://guild.xyz/networkLogos/polygon.svg",
+        ronin: "https://guild.xyz/networkLogos/ronin.svg",
+        sei: "https://guild.xyz/explorerLogos/seitrace.svg",
+        shape: "https://shapescan.xyz/assets/favicon/favicon-16x16.png",
+        solana: "https://avatars.githubusercontent.com/u/92743431?s=200&v=4",
+        soneium: "https://guild.xyz/networkLogos/soneium.webp",
+        unichain: "https://unichain.blockscout.com/assets/favicon/favicon-16x16.png",
+        zora: "https://guild.xyz/networkLogos/zora.svg"
+    };
+
+    const chainLogo = chainLogos[serverChain];
+    document.getElementById("serverChain").src = chainLogo;
+
+    const logoBase = 'data/logo';
+    const bannerBase = 'data/banner';
+    const extensions = ['png', 'jpg', 'jpeg'];
+
+    function tryLoadImage(base, callback) {
+        for (const ext of extensions) {
+            const path = `${base}.${ext}`;
+            const img = new Image();
+            img.src = path;
+
+            img.onload = () => callback(path);
+        }
+    }
+
+    tryLoadImage(logoBase, (path) => {
+        document.querySelectorAll('.logo-img').forEach(el => el.src = path);
+        const favicon = document.getElementById('favicon');
+        if (favicon) favicon.href = path;
+    });
+
+    tryLoadImage(bannerBase, (path) => {
+        const banner = document.querySelector('.banner-img');
+        if (banner) banner.src = path;
+    });
+
+    const textContainer = document.getElementById("ownershipText");
+    textContainer.innerHTML = isNFT ?
+        `Own a(n) <span class="replace-server">${serverName}</span> NFT` :
+        `Hold a minimum amount of <span class="replace-server">${ticker}</span>`;
